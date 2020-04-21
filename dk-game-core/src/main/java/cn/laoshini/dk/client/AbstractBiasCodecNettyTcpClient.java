ef46e124d41ee4f98b8480bc9c9b8b8a6e6e814e
fagarine <125767187@qq.com>
2020-04-21 22:40:37 +0800

    重新考虑AbstractBiasCodecNettyTcpClient的存在意义

--- dk-game-core/src/main/java/cn/laoshini/dk/client/AbstractBiasCodecNettyTcpClient.java
+++ dk-game-core/src/main/java/cn/laoshini/dk/client/AbstractBiasCodecNettyTcpClient.java
@@ -6,6 +6,9 @@ import cn.laoshini.dk.util.LogUtil;
 
 /**
  * 带有明确偏向编解码器的客户端抽象类，这样的类会使用指定的消息编解码器，所以来自父类的编解码器设置方法不再需要被用户单独调用
+ * <p>
+ * 目前来看，这个类存在的必要性不高，考虑在将来的版本中删除
+ * </p>
  *
  * @author fagarine
  */
@@ -14,20 +17,18 @@ public abstract class AbstractBiasCodecNettyTcpClient<S, M> extends AbstractNett
     @Override
     public final AbstractBiasCodecNettyTcpClient<S, M> setMessageEncoder(INettyMessageEncoder<M> messageEncoder) {
         if (messageEncoder() != null) {
-            LogUtil.error("消息编码器已存在，原编码器被覆盖！！！");
-        } else {
-            super.setMessageEncoder(messageEncoder);
+            LogUtil.error("消息编码器已存在，原编码器[{}]被[{}]覆盖！！！", messageDecoder().getClass(), messageEncoder.getClass());
         }
+        super.setMessageEncoder(messageEncoder);
         return this;
     }
 
     @Override
     public final AbstractBiasCodecNettyTcpClient<S, M> setMessageDecoder(INettyMessageDecoder<M> messageDecoder) {
         if (messageDecoder() != null) {
-            LogUtil.error("消息解码器已存在，原解码器被覆盖！！！");
-        } else {
-            super.setMessageDecoder(messageDecoder);
+            LogUtil.error("消息解码器已存在，原解码器[{}]被[{}]覆盖！！！", messageDecoder().getClass(), messageDecoder.getClass());
         }
+        super.setMessageDecoder(messageDecoder);
         return this;
     }
 }
